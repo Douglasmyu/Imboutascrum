2efef1ce905cb6c52f185c14fbd7a485e88bcaa4
Adam Louie <adumyumyum@csu.fullerton.edu>
2023-09-29 22:29:56 -0700

    CODE DONE MAYBE BETTER COMMENTS AND FORMATTING BUT IT WORKS; ALSO UPDATED README

--- README.md
+++ README.md
@@ -1,26 +1,9 @@
-## To Do
+# ASSIGNMENT 1
 
-- [x] create dicts for other identifiers: keywords(subset of id), delimiters/separators, operators
-- [x] implement isdigit() (do we have to use our fsm or can we just use .isnumeric())
-- [x] implement isID() //takes string and returns true if it passes as an id
-- [x] implement isreal()
-- [x] implement lexer() function - takes in buffer/lexeme and returns token + lexem
-- [x] create the main loop - reads through input file char by char from input.txt - adds chars into 'buffer' until hitting a separator/delimiter (also account for cases like >=) - once you hit a separator, call lexer function that returns token + lexeme - write token + lexeme into output file - [x] clear buffer to start recognizing next token
-- [x] write 3 test cases and include in zip file
-- [ ] account for comments [*, *]
-- [ ] account for negative numbers
+Adam Louie, Douglas Yu, Steven Liu
 
-## Questions:
+## TO DO
 
-- clarify the process for turning nfsms -> dfas -> code
-  (how do we handle empty/epsilon values) => i just used a "sink" value
-- clarify what to do with empty/epsilon values... sink states?
-- is # a delimiter or what
-
-## other things to do
-
-- double check nfsms and resulting dfas
-- double check list of keywords, separators, operators
-- create test cases (at least 3)
-- turn .py file into an .exe file
-- write documentation
+[ ] documentation
+[ ] convert py file to exe file for windows
+[ ] maybe some better comments in the source code
--- lex.py
+++ lex.py
@@ -5,58 +5,62 @@ SEPARATORS = {'(', ')', '{', '}', ',', ';', '#'}
 OPERATORS = {'=', '==', '!=', '>', '<', '<=', '>=', '+', '-', '*', '/'}
 DELIMITERS = {' ', '\n', '(', ')', '{', '}', ',', ';', '#'}
 
-# takes in a string and returns true or false if string is an indentifier  
+# DFA representation for identifiers returns true if is an identifier
 def isID(input):
-    # basically the final dfa shoved into a dict
+    # transition states 
     transition = {
         "letter": {
             "A": "B",
             "B": "C",
             "C": "C",
             "D": "C",
-            "E": "E" # sink state to account for empty
+            "E": "E" # sink state to account for empty state
         },
         "digit": {
             "A": "E",
             "B": "D",
             "C": "D",
             "D": "D",
-            "E": "E" # sink state to account for empty
+            "E": "E" # sink state to account for empty state
         }
     }
     
+    # declartion of the DFAs definitions
     accepting = ["B", "C"]
     starting_state = "A"
+    legal_letters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    legal_digits = set("0123456789")
     current = starting_state
     
-    # loops through input string and follows dfa table
+    # goes through the input string and follows the transition table
     for ch in input:
         # if input = l
-        if ch.isalpha():
+        if ch in legal_letters:
             current = transition["letter"][current]
         # if input = d
-        elif ch.isnumeric():
+        elif ch in legal_digits:
             current = transition["digit"][current]
         else:
         # neither letter or digit
-            return "False"
+            return False
     if current in accepting:
         return True
     else:
         return False
 
-# takes in input string and returns if is an int
+# DFA representation for integers returns true if an integer
 def isInt(input):
     transition = {
         "A": "B",
         "B": "B"
     }
-    legal_inputs = ['0','1','2','3','4','5','6','7','8','9']
+    
+    legal_digits = set("0123456789")
     accepting = "B"
     # setting current to starting state
     current = 'A'
     for ch in input:
-        if ch in legal_inputs:
+        if ch in legal_digits:
             current = transition[current]
         else: 
             return False
@@ -65,7 +69,7 @@ def isInt(input):
     else: 
         return False
 
-# takes in input string and returns true if follows real number re    
+# dfa representation for real numbers ex: 123.321 returns true if input is a real number 
 def isReal(input):
     transition = {
         "digit": {
@@ -83,11 +87,12 @@ def isReal(input):
             'E': 'E' # sink state
         }
     }
+    legal_digits = set("0123456789")
     accepting = 'D'
     # setting current to starting state
     current = 'A'
     for ch in input:
-        if ch.isnumeric():
+        if ch in legal_digits:
             current = transition['digit'][current]
         elif ch == '.':
             current = transition['dot'][current]
@@ -98,7 +103,7 @@ def isReal(input):
     else:
         return False
 
-# takes buffer as input and returns token and lexeme 
+# classifies a given lexeme into a predefined token
 def lexer(lexeme):
     if lexeme in KEYWORDS:
         token = "KEYWORDS"
@@ -114,67 +119,72 @@ def lexer(lexeme):
         token = "INTEGER"
     else:
         # if token is unreadable return false
-        token = False
+        token = "INVALID TOKEN"
     return token, lexeme
 
-with open('testCase3.txt', 'a') as f:
-    f.write(' ')
 
-# main code
-with open('testCase3.txt', 'r') as f, open('output.txt', 'w') as out:
-    
-    # string formatting and title creation
-    string_format = "{:<15} {}"
-    underline = '-' * 22
-    print(string_format.format("TOKEN", "LEXEME"))
-    print(underline)
-    out.write(string_format.format("TOKEN", "LEXEME") + '\n')
-    out.write(underline + '\n')
-    isComment = False
-    # read in initial char
-    ch = f.read(1)
 
-    # while there is a char to read
-    while ch:
-        # initialize empty buffer
-        buffer = ''
-        
-        # handling comments
-        if ch == '[':
-            nextCh = f.read(1)
-            if nextCh == '*':
-                isComment = True
-            else:
-                buffer += ch
-                ch = nextCh 
-                continue
-        elif isComment and ch == '*':
-            nextCh = f.read(1)
-            if nextCh == ']':
-                isComment = False
+testFiles = ["testCase1.txt", "testCase2.txt", "testCase3.txt"]
+outputFiles = ["output1.txt", "output2.txt", "output3.txt" ]
+for i in range(3):
+    with open(testFiles[i], 'a') as f:
+        f.write(' ')
+# reads through all of the test files and outputs them into their respective output files
+for i in range(3):
+    with open(testFiles[i], 'r') as f, open(outputFiles[i], 'w') as out:
+        # string formatting and title creation
+        string_format = "{:<15} {}"
+        underline = '-' * 22
+        print(string_format.format("TOKEN", "LEXEME"))
+        print(underline)
+        out.write(string_format.format("TOKEN", "LEXEME") + '\n')
+        out.write(underline + '\n')
+        isComment = False
+        # read in initial char
+        ch = f.read(1)
+
+        # while there is a char to read
+        while ch:
+            # initialize empty buffer
+            buffer = ''
+            
+            # handling comments
+            if ch == '[':
+                nextCh = f.read(1)
+                if nextCh == '*':
+                    isComment = True
+                else:
+                    buffer += ch
+                    ch = nextCh 
+                    continue
+            elif isComment and ch == '*':
+                nextCh = f.read(1)
+                if nextCh == ']':
+                    isComment = False
+                    ch = f.read(1)
+                    continue
+            if isComment:
                 ch = f.read(1)
                 continue
-        if isComment:
-            ch = f.read(1)
-            continue
-            
-        # read in ch until hitting delim
-        while ch not in DELIMITERS:
-            buffer += ch
-            ch = f.read(1)
-        # if there is something in buffer call lexer for token and print
-        if buffer:
-            token = lexer(buffer)[0]
-            lexeme = lexer(buffer)[1]
-            if token:
-                out.write(string_format.format(token, lexeme) + '\n')
-                print(string_format.format(token, lexeme))
-        # if current char is a separator print separator
-        if ch in SEPARATORS:
-            token = lexer(ch)[0]
-            if token:
-                out.write(string_format.format(token, ch) + '\n')
-                print(string_format.format(token, ch))
                 
-        ch = f.read(1)
+            # read in ch until hitting delim
+            while ch not in DELIMITERS:
+                buffer += ch
+                ch = f.read(1)
+            # if there is something in buffer call lexer for token and print
+            if buffer:
+                token = lexer(buffer)[0]
+                lexeme = lexer(buffer)[1]
+                if token:
+                    out.write(string_format.format(token, lexeme) + '\n')
+                    print(string_format.format(token, lexeme))
+            # if current char is a separator print separator
+            if ch in SEPARATORS:
+                token = lexer(ch)[0]
+                if token:
+                    out.write(string_format.format(token, ch) + '\n')
+                    print(string_format.format(token, ch))
+                    
+            ch = f.read(1)
+
 
